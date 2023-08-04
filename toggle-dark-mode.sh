1d2b44c08572c495fb1d7faead4da21f21c3512c
Alexander Celeste <alex@tenseg.net>
2023-08-04 15:30:08 -0500

    toggle dark mode script

---
+++ toggle-dark-mode.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env fish
+
+# myip
+#
+# Toggles dark mode
+#
+# @raycast.schemaVersion 1
+# @raycast.title Toggle Dark Mode
+# @raycast.mode silent
+# @raycast.icon 🌗
+# @raycast.packageName Personal Scripts
+
+if test (defaults read -g AppleInterfaceStyle | tr -d '\r') = "Dark"; then
+	echo "Dark mode is off"
+	osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to false'
+else
+	echo "Dark mode is on"
+	osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true'
+end
